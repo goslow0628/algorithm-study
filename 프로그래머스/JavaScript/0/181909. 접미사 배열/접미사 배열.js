97d26fcf47975a872597b4e4396d3640b7206555
Seongchan Park <goslow0628@gmail.com>
2026-03-06 23:38:50 +0900

    [level 0] Title: 접미사 배열, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/JavaScript/0/181909. 접미사 배열/접미사 배열.js
@@ -0,0 +1,12 @@
+function solution(my_string) {
+  let answer = [];
+
+  for (let i = 0; i < my_string.length; i++) {
+    let str = my_string.slice(i);
+    answer.push(str);
+  }
+
+  answer.sort();
+
+  return answer;
+}
